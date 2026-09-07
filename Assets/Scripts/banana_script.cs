bd1851344fcf1a29d4e8c76093f68fcc1477b754
JustinBench <d00188916@utahtech.edu>
2026-09-07 13:32:31 -0600

    first-assignment-done
    The monsters will dance if there's a banana nearby

--- Assets/Scripts/banana_script.cs
+++ Assets/Scripts/banana_script.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class banana_script : MonoBehaviour
 {
+    public float pushDistance = 1.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision detected!");
+        if (other.CompareTag("Player"))
+        {
+            Vector3 pushDirection = transform.position - other.transform.position;
+            pushDirection.y = 0f;
+            pushDirection.Normalize();
+
+            transform.position += pushDirection * pushDistance;
+        }
     }
 }
